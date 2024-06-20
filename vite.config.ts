6541f53479ce65241510693d423e61dd75b4b72d
KaiHotz <kaihotz@me.com>
2024-06-21 00:40:31 +0400

    fonts error fixed

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite';
 import svgr from 'vite-plugin-svgr';
+import { resolve } from 'path';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr({ include: '**/*.svg' })],
+  resolve: {
+    alias: [
+      {
+        find: '../fonts',
+        replacement: resolve(__dirname, './src/styles/fonts'),
+      },
+    ],
+  },
 });
