27d70d54ec0db9ee2ab5a7b1a7e4c4f9d98c76d7
Kai Hotz <kaihotz@me.com>
2024-12-06 23:45:02 +0400

    eslint config ecmaversion set to latest

--- eslint.config.js
+++ eslint.config.js
@@ -11,13 +11,13 @@ import tseslint from 'typescript-eslint';
 import pluginImport from 'eslint-plugin-import';
 export default tseslint.config(
   {
-    ignores: ['dist', '.git', '.npmrc', '.yarnrc', 'coverage', 'storybook-static', '.yarn'],
+    ignores: ['dist', '.git', '.npmrc', '.yarnrc', 'coverage', 'storybook-static', '.yarn', 'node_modules'],
   },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended, configPrettier],
     files: ['**/*.{ts,tsx}', '**/*.{js,jsx}'],
     languageOptions: {
-      ecmaVersion: 2020,
+      ecmaVersion: 'latest',
       globals: globals.browser,
       parser: tseslint.parser,
       parserOptions: {
