9d1d008b30189f6cb6db95d58d46c02392471b0d
Kai Hotz <kaihotz@me.com>
2025-12-14 09:14:24 +0400

    dependencies updated

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,13 +2,7 @@ import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
   'stories': ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-  'addons': [
-    '@storybook/addon-designs',
-    '@vueless/storybook-dark-mode',
-    '@storybook/addon-docs',
-    '@storybook/addon-vitest',
-    '@storybook/addon-a11y'
-  ],
+  'addons': ['@storybook/addon-designs', '@vueless/storybook-dark-mode', '@storybook/addon-docs', '@storybook/addon-vitest'],
   'framework': {
     'name': '@storybook/react-vite',
     'options': {},
